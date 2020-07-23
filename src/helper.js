75e3d22472274f7373bf7b7f8730ff1155ab59bb
mfranz <franz08@email.franklin.edu>
2020-07-23 19:04:27 -0400

    remove log

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,4 @@
 export const generateData = (width, height) => {
-    console.log({height});
     return Array(25).fill(0).map(() => ([
         Math.floor(Math.random() * width) + 1,
         Math.floor(Math.random() * height) + 1
