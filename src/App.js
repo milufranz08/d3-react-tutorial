0543d1256cdd2b04b5c86490311e9aaa9194ee13
mfranz <franz08@email.franklin.edu>
2020-07-25 15:31:47 -0400

    Switch hook

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { generateData } from './helper';
 import './App.css';
-
-// custom hook that runs some logic every certain time
-const useInterval = (callback, delay) => {
-  const savedCallback = useRef();
-
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  useEffect(() => {
-    const tick = () => {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
-};
 
 function App() {
   const [data, setData] = useState(generateData());
 
-  useInterval(() => {
-    const newData = generateData()
-    setData(newData)
-  }, 2000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newData = generateData();
+      setData(newData);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="App">
